8c05eb0d1fdd09e65a0d31cdf49bdaf412990123
gar354 <115901590+gar354@users.noreply.github.com>
2024-12-06 13:46:55 -0800

    refactor changes

--- src/main.py
+++ src/main.py
@@ -3,96 +3,86 @@
 from vex import *
 import math
 
+WHEEL_CIRC = 4.5 * 2 * math.pi
+
 brain = Brain()
-cntrl = Controller()
-motor1=Motor(Ports.PORT1)
-motor2=Motor(Ports.PORT10)
-odony=Rotation(Ports.PORT12)
-wheelcirc = 13
-robotxposition=0
-globaltheta=math.pi/2
+ctrl = Controller()
+motor_r=Motor(Ports.PORT1)
+motor_l=Motor(Ports.PORT10)
+motor_t=Rotation(Ports.PORT12)
+
+# set to 90 degrees
+global_rot=math.pi/2
+
 wheelbase=10
-xglobal=0
-yglobal=0
-previoustheta=0
-localradius=0
-avgtheta=0
+x_global=0
+y_global=0
+prev_theta=0
+local_arc_rad=0
+avg_theta=0
 
 
 
 
-def Xfrompolar(r,theta):
+def x_from_polar(r,theta):
     x = r * math.cos(theta)
     return float(x)
-def Yfrompolar(r,theta):
+def y_from_polar(r,theta):
     y = r * math.sin(theta)
     return float(y)
 
-def Rfromcartesian(x,y):
+def r_from_cartisian(x,y):
     r = math.sqrt(x**2 + y**2) 
     return float(r)
-def Thetafromcartesian(x,y):
+def r_from_cartesian(x,y):
     theta = math.atan2(y,x)
     return float(theta)
     
 def user_control():
-    xvector=0
-    yvector=0
-    global globaltheta, xglobal,yglobal,averagetheta,localradius,avgtheta
-    odony.reset_position()
-    motor1.reset_position()
-    motor2.reset_position()
+    arc_x=0
+    arc_y=0
+    global global_rot, x_global,y_global,averagetheta,local_arc_rad,avg_theta
+    motor_t.reset_position()
+    motor_r.reset_position()
+    motor_l.reset_position()
     while True:
-        ypos1=-(motor2.position()/360)*wheelcirc 
-        ypos12=(motor1.position()/360)*wheelcirc 
-        xpos1 =-(odony.position()/360)*wheelcirc 
-        wait(100)
-        ypos2=-(motor2.position()/360)*wheelcirc
-        ypos22=(motor1.position()/360)*wheelcirc 
-        xpos2 =-(odony.position()/360)*wheelcirc 
+        ypos1=-(motor_l.position()/360)*WHEEL_CIRC 
+        ypos12=(motor_r.position()/360)*WHEEL_CIRC 
+        xpos1 =-(motor_t.position()/360)*WHEEL_CIRC 
+        wait(10)
+        ypos2=-(motor_l.position()/360)*WHEEL_CIRC
+        ypos22=(motor_r.position()/360)*WHEEL_CIRC 
+        xpos2 =-(motor_t.position()/360)*WHEEL_CIRC 
 
         deltay1=ypos2-ypos1
         deltay2=ypos22-ypos12
         deltax=xpos2-xpos1
 
 
-        deltatheta=((deltay1-deltay2)/wheelbase)
-        previoustheta=globaltheta
-        globaltheta+=deltatheta
-
-        #everything above this works - hooray!
-
-        #this actually works apparently lol
-        try:
-            radius = (deltay1/deltatheta)+(5)
-        except ZeroDivisionError:
-            radius = 0
-
+        delta_theta=((deltay1-deltay2)/wheelbase)
+        previoustheta=global_rot
+        global_rot+=delta_theta
 
         
-        #this works for going straight
-        if deltatheta==0:
-            yglobal+=deltay1
-        
-        #rotations
-        
-        else:
-            
-            xvector=2*((deltax/deltatheta)+6.25)*(math.sin(deltatheta/2))
-            yvector=2*((deltay1/deltatheta)+4.5)*(math.sin(deltatheta/2))
+        #this does not work for going straight
+        if delta_theta==0:
+            y_global+=2*((deltay1/0.0000000000001)+4.5)*(math.sin(delta_theta/2))
+        # arcs
+        elif (deltay1>0 and deltay2>0) or (deltay1<0 and deltay2<0):
+            arc_x=2*((deltax/delta_theta)+6.25)*(math.sin(delta_theta/2))
+            arc_y=2*((deltay1/delta_theta)+4.5)*(math.sin(delta_theta/2))
 
             
-            avgtheta=previoustheta+(deltatheta/2)
+            avg_theta=previoustheta+(delta_theta/2)
 
-            localradius=Rfromcartesian(xvector,yvector)
-            localtheta=Thetafromcartesian(xvector,yvector)
-            localtheta=localtheta-avgtheta
-            xglobal+=Xfrompolar(localradius,localtheta)
-            yglobal+=Yfrompolar(localradius,localtheta)
+            local_arc_rad=r_from_cartisian(arc_x,arc_y)
             
-
-        
-        print(xglobal,xglobal)
-
+            # Rotating to match global pos
+            local_theta=r_from_cartesian(arc_x,arc_y)
+            local_theta=local_theta-avg_theta
+            
+            x_global+=x_from_polar(local_arc_rad,local_theta)
+            y_global+=y_from_polar(local_arc_rad,local_theta)
+        print(x_global,x_global)
 
 user_control()

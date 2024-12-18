b7cdcee1791e0f178e8494808332f10f56612d35
AugustusSchaible <augustus.schaible@bush.edu>
2024-12-18 09:53:17 -0800

    fixed everything

--- src/BezierDrive.py
+++ src/BezierDrive.py
@@ -2,10 +2,14 @@ from vex import *
 
 brain=Brain()
 controller_1 = Controller(PRIMARY)
-left_drive_motor_a = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
-left_drive_motor_b = Motor(Ports.PORT3, GearSetting.RATIO_6_1, False)
-right_drive_motor_a = Motor(Ports.PORT4, GearSetting.RATIO_6_1, True)
-right_drive_motor_b = Motor(Ports.PORT5, GearSetting.RATIO_6_1, True)
+left_drive_motor_a = Motor(Ports.PORT2, GearSetting.RATIO_6_1, True)
+left_drive_motor_b = Motor(Ports.PORT3, GearSetting.RATIO_6_1, True)
+left_drive_motor_c = Motor(Ports.PORT4, GearSetting.RATIO_6_1, True)
+
+right_drive_motor_a = Motor(Ports.PORT15, GearSetting.RATIO_6_1, False)
+right_drive_motor_b = Motor(Ports.PORT16, GearSetting.RATIO_6_1, False)
+right_drive_motor_c = Motor(Ports.PORT17, GearSetting.RATIO_6_1, False)
+
 right_drive = MotorGroup(right_drive_motor_a, right_drive_motor_b)
 left_drive = MotorGroup(left_drive_motor_a, left_drive_motor_b)
 doinker = Motor(Ports.PORT7, GearSetting.RATIO_36_1, False)
@@ -21,8 +25,10 @@ def when_started1():
     #graph: https://www.desmos.com/calculator/vigixi45si
     right_drive.set_stopping(COAST)
     left_drive.set_stopping(COAST)
+    ithrottle = 0
+    iturn = 0
     global deadzone
-    deadzone = 10
+    deadzone = 0.1
     x1 = 0
     y1 = 0
     x2 = 70
@@ -32,18 +38,14 @@ def when_started1():
     x4 = 100 
     y4 = 100
     t= 0
-    x5 = []
-    y5 = []
 
     #velocity multiplier - speed percent or ratio (0-1)
     global velocity_multiplier
     velocity_multiplier = 1
 
     while True:
-        global deadzone
         #assign initial stick values
-        ithrottle = (controller_1.axis3.position() / 100)
-        iturn = (controller_1.axis1.position() / 100)
+        
         global throttle
         global turn 
 
@@ -51,25 +53,28 @@ def when_started1():
         throttle = ((1-ithrottle)**3 * x1 + 3*(1-ithrottle)**2*ithrottle * x2 + 3*(1-ithrottle)*ithrottle**2 * x3 + ithrottle**3 * x4)
         turn = ((1-iturn)**3 * x1 + 3*(1-iturn)**2*iturn * x2 + 3*(1-iturn)*iturn**2 * x3 + iturn**3 * x4)
 
+        #divide stick values by 100 so 0<t<1
+        ithrottle = (controller_1.axis3.position() / 100)
+        iturn = (controller_1.axis1.position() / 100)
         #apply bezier curve to stick inputs if outside of deadzone
         if ithrottle > deadzone: 
             global throttle
             throttle = ((1-ithrottle)**3 * x1 + 3*(1-ithrottle)**2*ithrottle * x2 + 3*(1-ithrottle)*ithrottle**2 * x3 + ithrottle**3 * x4)
         if ithrottle < -deadzone: 
             global throttle
+            ithrottle = -1 * ithrottle
             throttle = -1 * ((1-ithrottle)**3 * x1 + 3*(1-ithrottle)**2*ithrottle * x2 + 3*(1-ithrottle)*ithrottle**2 * x3 + ithrottle**3 * x4)
-        if iturn > deadzone: 
+        if iturn > deadzone and (ithrottle > deadzone or ithrottle < -deadzone): 
             global turn
             turn = ((1-iturn)**3 * x1 + 3*(1-iturn)**2*iturn * x2 + 3*(1-iturn)*iturn**2 * x3 + iturn**3 * x4)
-        if iturn < -deadzone: 
-             global turn 
-             turn = -1 * ((1-iturn)**3 * x1 + 3*(1-iturn)**2*iturn * x2 + 3*(1-iturn)*iturn**2 * x3 + iturn**3 * x4)
+        if iturn < -deadzone and (ithrottle > deadzone or ithrottle < -deadzone): 
+            global turn 
+            iturn = -1 * iturn
+            turn = -1 * ((1-iturn)**3 * x1 + 3*(1-iturn)**2*iturn * x2 + 3*(1-iturn)*iturn**2 * x3 + iturn**3 * x4)
+            print(turn)
 
+        
         #assign remapped stick positions to motor speeds
-        global throttle
-        global turn
-        global left
-        global right
         left = throttle + turn
         right = throttle - turn 
 
@@ -89,7 +94,10 @@ def when_started1():
             left_drive.set_velocity(velocity_multiplier*left, PERCENT)
 
         #spin motors if stick is out of deadzone
-        if throttle > deadzone or throttle < -deadzone: 
+        if throttle > deadzone:
+            right_drive.spin(FORWARD)
+            left_drive.spin(FORWARD)
+        if throttle < -deadzone:
             right_drive.spin(FORWARD)
             left_drive.spin(FORWARD)
         #stop motors when no inputs or in deadzone
@@ -97,14 +105,18 @@ def when_started1():
             right_drive.stop()
             left_drive.stop()
         #print values for debugging
+        
         print('turn: ')
-        print(turn)
+        print(iturn)
         print('throttle: ')
-        print(throttle)
+        print(ithrottle)
         print('left: ')
         print(left)
         print('right: ')
         print(right)
+        wait(30,MSEC)
+        
+        
 
 when_started1()
 
